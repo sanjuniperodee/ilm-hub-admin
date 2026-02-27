5bbe2d5a29b954c4efe037350d8b17264d5b53e1
sanjuniperodee <87779571856b@gmail.com>
2026-02-27 22:15:46 +0500

    redesign

--- src/pages/IslamAllahNamesPage.tsx
+++ src/pages/IslamAllahNamesPage.tsx
@@ -21,8 +21,6 @@ import {
   Typography,
 } from '@mui/material'
 import { Add, Audiotrack, Delete, Edit, Refresh } from '@mui/icons-material'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Switch from '@mui/material/Switch'
 import {
   getIslamAllahNames,
   createIslamAllahName,
